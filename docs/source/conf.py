d73c9635a3fe9284479cdd29b9e00aadf80a0b03
Dave Dittrich <dittrich@u.washington.edu>
2016-05-16 04:19:43 -0700

    Add bumpversion support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ copyright = u'2014, 2015 David Dittrich'
 # built documents.
 #
 # The short X.Y version.
-version = '2.1.2'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
